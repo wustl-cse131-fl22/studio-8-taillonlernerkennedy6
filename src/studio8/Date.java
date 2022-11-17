38fea17fd8c9a36aff886977723d40cb968c6dbf
gigitaillon <gigitaillon@gigis-mbp.dhcp.wustl.edu>
2022-11-17 12:54:51 -0600

    Studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,42 @@
 package studio8;
 
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
+	private int month;
+	private int day;
+	private int year;
 
+	public static void main(String[] args) {
+		LinkedList<Date> list = new LinkedList<Date>();
 
-    public static void main(String[] args) {
+	}
 
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
+
+	public Date(int m, int d, int y) {
+		month = m;
+		day = d;
+		year = y;
+	}
+
+	public String toString() {
+		return this.month + "/" + this.day + "/" + this.year;
+	}
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,52 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+	private int hour;
+	private int minute;
+	private boolean hourFormat;
 
 	public static void main(String[] args) {
- 
-    	
+		Time time1 = new Time(11, 53, true);
+		Time time2 = new Time(23, 14, true);
+		Time time3 = new Time(7, 32, false);
+		Time time4 = new Time(16, 28, false);
     }
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, hourFormat, minute);
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && hourFormat == other.hourFormat && minute == other.minute;
+	}
+
+	public Time(int h, int m, boolean hF) {
+		this.hour = h;
+		this.minute = m;
+		this.hourFormat = hF;
+		if(hF == true) {
+			//12-hour time
+			this.hourFormat = true;
+		}
+	}
+	
+	public String toString() {
+		if(this.hourFormat == true) {
+			return this.hour + ":" + this.minute;
+		}
+		else {
+			return hour - 12 + ":" + minute;
+		}
+	}
 }
